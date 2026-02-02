4aaa2c808b0527c3495a315803d791ef98bdfb01
baudelotphilippe <philippe@baudelot.eu>
2026-02-02 14:24:10 +0100

    serializer

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -5,13 +5,12 @@ namespace App\Controller;
 use App\Dto\UserDto;
 use App\Form\UserType;
 use App\Service\UserService;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Component\Serializer\SerializerInterface;
-use Symfony\Component\HttpKernel\Attribute\MapRequestPayload;
 use Symfony\Component\Validator\Validator\ValidatorInterface;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 final class UserController extends AbstractController
 {
@@ -32,9 +31,16 @@ final class UserController extends AbstractController
     }
 
     #[Route('/userFromJson', name: 'app_user_Json')]
-    public function indexJson(UserService $userService, #[MapRequestPayload] UserDto $user): Response
+    public function indexJson(Request $request, UserService $userService, ValidatorInterface $validator, SerializerInterface $serializer): Response
     {
+        $user = $serializer->deserialize($request->getContent(), UserDto::class, 'json');
+        $errors = $validator->validate($user);
+
+        if (count($errors) > 0) {
+            return $this->json($errors, 400);
+        }
         $userService->create($user);
+
         return $this->render('user/index.html.twig', [
             'form' => $user,
         ]);
